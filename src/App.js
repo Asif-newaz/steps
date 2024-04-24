09efe09bc835ac0cdc415adef3733919ae2a3c3f
Asif-newaz <asif.newaz0093@gmail.com>
2024-04-25 00:03:09 +0600

    feat: work react handling event

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ const messages = [
 function App() {
   const step = 2;
 
+  const handlePrevious = () => {
+    alert("This is previous button!");
+  };
+
+  const handleNext = () => {
+    alert("This is next button!");
+  };
+
   return (
     <div className="steps">
       <div className="numbers">
@@ -20,10 +28,16 @@ function App() {
         Step {step}: {messages[step - 1]}
       </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
